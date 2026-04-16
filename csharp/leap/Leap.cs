aa7e47c7ff2d5de89805c70724ff833f97e2109d
abdallaha <a.alqudah3@gmail.com>
2026-04-16 13:51:47 +0200

    feat: solve Leap Year challenge (updated)

--- csharp/leap/Leap.cs
+++ csharp/leap/Leap.cs
@@ -19,4 +19,15 @@ public static bool IsLeapYear(int year)
     // Rule 3: Otherwise, any year divisible by 4 is a leap year
     return year % 4 == 0;
 }
+---------------------------------------------------------------------------
+public static bool IsLeapYear(int year)
+{
+    return (year % 4, year % 100, year % 400) switch
+    {
+        (_, 0, 0) => true,
+        (_, 0, _) => false,
+        (0, _, _) => true,
+        _ => false,
+    };
+}
 */
