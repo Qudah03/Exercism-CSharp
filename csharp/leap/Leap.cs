ff925cedc6ef0969190dd2fe7069dad5c536c54a
abdallaha <a.alqudah3@gmail.com>
2026-04-16 13:48:18 +0200

    feat: solve Leap Year challenge

---
+++ csharp/leap/Leap.cs
@@ -0,0 +1,22 @@
+public static class Leap
+{
+    public static bool IsLeapYear(int year)
+    {
+        // Divisible by 4 AND (NOT divisible by 100 OR divisible by 400)
+    return (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0);
+    }
+}
+
+/*
+public static bool IsLeapYear(int year)
+{
+    // Rule 1: Every 400 years is ALWAYS a leap year
+    if (year % 400 == 0) return true;
+
+    // Rule 2: If it's a century but not the 400th, it's NOT a leap year
+    if (year % 100 == 0) return false;
+
+    // Rule 3: Otherwise, any year divisible by 4 is a leap year
+    return year % 4 == 0;
+}
+*/
