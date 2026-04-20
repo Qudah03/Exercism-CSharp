eaebab99cc8feaba28f12e432ba28720f80f041b
abdallaha <a.alqudah3@gmail.com>
2026-04-20 09:41:31 +0200

    feat: solve difference of squares using LINQ and Formula Implementation

--- csharp/difference-of-squares/DifferenceOfSquares.cs
+++ csharp/difference-of-squares/DifferenceOfSquares.cs
@@ -4,14 +4,15 @@ public static class DifferenceOfSquares
 {
     public static int CalculateSquareOfSum(int max)
     {
-        var sum = Enumerable.Range(1, max).Sum();
+        // Enumerable.Range(1, max) creates the list: 1, 2, 3...
+        int sum = Enumerable.Range(1, max).Sum();
         return sum * sum;
     }
 
     public static int CalculateSumOfSquares(int max)
     {
-        var sum = Enumerable.Range(1,max).Select(x => x * x).Sum();
-        return sum;
+        return Enumerable.Range(1,max).Select(x => x * x).Sum();
+       
     }
 
     public static int CalculateDifferenceOfSquares(int max)
