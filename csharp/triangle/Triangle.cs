55e3a1b56c954debb9b849dfefbeb5b87840350b
abdallaha <a.alqudah3@gmail.com>
2026-04-20 13:35:55 +0200

    feat: complete Triangle exercise

---
+++ csharp/triangle/Triangle.cs
@@ -0,0 +1,38 @@
+public static class Triangle
+{
+    public static bool IsScalene(double side1, double side2, double side3)
+    {
+        if (side1 != side2 && side2 != side3 && side1 != side3)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static bool IsIsosceles(double side1, double side2, double side3) 
+    {
+        if (side1 == side2 || side2 == side3 || side1 == side3)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static bool IsEquilateral(double side1, double side2, double side3) 
+    {
+        if (side1 == side2 && side2 == side3)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
